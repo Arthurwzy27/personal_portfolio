cc72e0b75cad1da9e5c37270aaae5b7e7d290d17
Arthur Picard <arthur.picard7@gmail.com>
2021-12-07 12:21:13 +0100

    Features: Routes for About, Projects and Contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'projects', to: 'pages#projects', as: :projects
+  get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

5852a3fe22b00a0d6cb9eea9b218a1806ad12bab
Arthur Picard <arthur.picard7@gmail.com>
2021-12-07 12:26:20 +0100

    Features: All navbar items link to correct path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
-  get 'projects', to: 'pages#projects', as: :projects
+  get 'project', to: 'pages#project', as: :project
   get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
